d30d530b3478decf7e6c4bb87c325c7b4009ff39
”Lilia2002” <l.predko@list.ru>
2021-08-10 15:03:50 +0700

    Creating a home page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Entity\Order;
+use App\Entity\Product;
+use App\Form\Type\CategoryType;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\Type\ProductType;
+
+class HomeController extends AbstractController
+{
+    public function homepage(ProductRepository $productRepository)
+    {
+        return $this->render('product/homepage.html.twig', [
+            'products' => $productRepository->findProductsLimited(),
+        ]);
+    }
+
+}
